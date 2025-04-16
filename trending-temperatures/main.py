6de28be3577e170f9ba81039ff2adb151d591a1d
linda_a <larmstrongnz@gmail.com>
2025-04-17 09:06:29 +1200

    Completed histogram exercise

--- trending-temperatures/main.py
+++ trending-temperatures/main.py
@@ -6,8 +6,8 @@ temp = [28,32,31,40,45,55,60,65,54,43,34,30]
 plt.plot(month, temp, color="#03fc45")
 
 plt.xlabel("Months of Year", fontsize=16)
-plt.ylabel("Temperature in Degrees Celcius", fontsize=16)
+plt.ylabel("Temperature in Fahrenheit", fontsize=16)
 
-plt.title("Average Monthly Temperatures in North Pole, Alaska")
+plt.title("Average Monthly Temperatures for 2018 in North Pole, Alaska")
 
 plt.savefig("alaska_temps.png")
