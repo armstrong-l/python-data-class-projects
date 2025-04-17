3e4a2b0ffddc9bf7f3a905b9e74d74345d0b005d
linda_a <larmstrongnz@gmail.com>
2025-04-18 10:02:10 +1200

    Completed first exercise tips data

--- tips-and-group-size/main.py
+++ tips-and-group-size/main.py
@@ -6,3 +6,18 @@ with open("tips.csv", "r") as file:
   headers = next(data)
   data_list = list(data)
   data_numpy = np.array(data_list)
+
+
+size = data_numpy[:,6]
+
+tips = np.array(data_numpy[:,1], dtype=float)
+
+bills = np.array(data_numpy[:,0], dtype=float)
+
+# print(size)
+# print(tips)
+# print(bills)
+
+tip_percentages=(tips/bills*100)
+
+print(tip_percentages)
