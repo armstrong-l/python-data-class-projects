649d1eec20f00992d90abdafe613416e6a36eebd
linda_a <larmstrongnz@gmail.com>
2025-04-25 12:04:21 +1200

    Completed choose own data visualization exercise

--- choose-your-own-data-visualization-adventure/main.py
+++ choose-your-own-data-visualization-adventure/main.py
@@ -2,7 +2,16 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-with open("books.csv","r") as datafile:
-    data = pd.read_csv(datafile,delimiter=",")
+data = pd.read_csv("books.csv", delimiter=",")
 
-print(data["language_code"])
+reviewsCount = data["text_reviews_count"]
+averageRating = data["average_rating"]
+
+plt.scatter(averageRating, reviewsCount, color="green")
+
+plt.xlabel("Average Rating")
+plt.ylabel("Number of Reviews")
+plt.title("Average Rating Compared to Number of Reviews")
+
+plt.savefig("rating_review_graph.png")
+
